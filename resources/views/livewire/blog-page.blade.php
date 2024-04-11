8f85c36488af7d0dc410bf438a3a7aec180d96fc
Dipak <drt81346@gmail.com>
2024-04-11 19:22:25 +0530

    Add New File

--- resources/views/livewire/blog-page.blade.php
+++ resources/views/livewire/blog-page.blade.php
@@ -3,11 +3,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-8 mx-auto text-center">
-                    <h2 class="mb-3 text-capitalize">Blog</h2>
+                    <h2 class="mb-3 text-capitalize">Dipak</h2>
                     <ul class="list-inline breadcrumbs text-capitalize" style="font-weight:500">
                         <li class="list-inline-item"><a href="index.html">Home</a>
                         </li>
-                        <li class="list-inline-item">/ &nbsp; <a href="blog.html">Blog</a>
+                        <li class="list-inline-item">/ &nbsp; <a href="blog.html">Dipak</a>
                         </li>
                     </ul>
                 </div>
